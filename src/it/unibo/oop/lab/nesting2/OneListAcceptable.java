dcccb6caccb677694658c7d7b23b20b7774830e7
alebrasi <alessandro.brasini1@gmail.com>
2020-11-12 20:02:42 +0100

    Finished nesting2

--- src/it/unibo/oop/lab/nesting2/OneListAcceptable.java
+++ src/it/unibo/oop/lab/nesting2/OneListAcceptable.java
@@ -1,11 +1,48 @@
 package it.unibo.oop.lab.nesting2;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class OneListAcceptable<T> implements Acceptable<T> {
 
+    private final List<T> list;
+    
+    public OneListAcceptable(List<T> list) {
+        this.list = new ArrayList<>(list);
+    }
+    
 	@Override
 	public Acceptor<T> acceptor() {
-		
-		return null;
+		return new AcceptorImpl();
 	}
+	
+	private class AcceptorImpl implements Acceptor<T>{
+	    
+	    private int index;
+	    
+	    private AcceptorImpl(){
+	        index = 0;
+	    }
+	    
+        @Override
+        public void accept(T newElement) throws ElementNotAcceptedException {
+            if(indexIsInRange() && list.get(index).equals(newElement)) {
+                index++;
+            }else {
+                throw new ElementNotAcceptedException(newElement);
+            }
+        }
+        
+        private boolean indexIsInRange() {
+            return index < list.size(); 
+        }
 
+        @Override
+        public void end() throws EndNotAcceptedException {
+            if(indexIsInRange()) {
+                throw new EndNotAcceptedException();
+            }
+        }
+	    
+	}
 }
